419e0492a353826f9b962c9543a21f096dd9726e
viveroa2291 <viveroa2291@uwec.edu>
2022-11-21 17:00:25 -0600

    Changes
    Making Changes
    - Created sections for the links

---
+++ home.js
@@ -0,0 +1,108 @@
+function finance() {
+    var finance = document.getElementById("finance");
+    var code = document.getElementById("code");
+    var social = document.getElementById("social");
+    var sponsors = document.getElementById("sponsors");
+    var connect = document.getElementById("connect");
+    var socialMediaHeaders = document.getElementById("social-media-headers");
+
+    var block = document.getElementById("block");
+    var block2 = document.getElementById("block2");
+    var block3 = document.getElementById("block3");
+    var block4 = document.getElementById("block4");
+
+    block.style.backgroundColor = "green";
+    block2.style.backgroundColor = "green";
+    block3.style.backgroundColor = "green";
+    block4.style.backgroundColor = "green";
+
+    connect.style.display = "none";
+    finance.style.display = "block";
+    code.style.display = "none";
+    social.style.display = "none";
+    sponsors.style.display = "none";
+    socialMediaHeaders.style.display = "none";
+}
+function code() {
+    var finance = document.getElementById("finance");
+    var code = document.getElementById("code");
+    var social = document.getElementById("social");
+    var sponsors = document.getElementById("sponsors");
+    var connect = document.getElementById("connect");
+    var socialMediaHeaders = document.getElementById("social-media-headers");
+
+
+    var block = document.getElementById("block");
+    var block2 = document.getElementById("block2");
+    var block3 = document.getElementById("block3");
+    var block4 = document.getElementById("block4");
+
+    block.style.backgroundColor = "purple";
+    block2.style.backgroundColor = "purple";
+    block3.style.backgroundColor = "purple";
+    block4.style.backgroundColor = "purple";
+
+    connect.style.display = "none";
+    finance.style.display = "none";
+    code.style.display = "block";
+    social.style.display = "none";
+    sponsors.style.display = "none";
+    socialMediaHeaders.style.display = "none";
+}
+function sponsors() {
+    var finance = document.getElementById("finance");
+    var code = document.getElementById("code");
+    var social = document.getElementById("social");
+    var sponsors = document.getElementById("sponsors");
+    var connect = document.getElementById("connect");
+    var socialMediaHeaders = document.getElementById("social-media-headers");
+
+    var block = document.getElementById("block");
+    var block2 = document.getElementById("block2");
+    var block3 = document.getElementById("block3");
+    var block4 = document.getElementById("block4");
+
+    block.style.backgroundColor = "yellow";
+    block2.style.backgroundColor = "yellow";
+    block3.style.backgroundColor = "yellow";
+    block4.style.backgroundColor = "yellow";
+
+    connect.style.display = "none";
+    finance.style.display = "none";
+    code.style.display = "none";
+    social.style.display = "none";
+    sponsors.style.display = "block";
+    socialMediaHeaders.style.display = "none";
+}
+function connect() {
+    var finance = document.getElementById("finance");
+    var code = document.getElementById("code");
+    var social = document.getElementById("social");
+    var sponsors = document.getElementById("sponsors");
+    var connect = document.getElementById("connect");
+    var socialMediaHeaders = document.getElementById("social-media-headers");
+
+
+    finance.style.display = "none";
+    connect.style.display = "block";
+    code.style.display = "none";
+    social.style.display = "none";
+    sponsors.style.display = "none";
+    socialMediaHeaders.style.display = "none";
+}
+function social() {
+    var finance = document.getElementById("finance");
+    var code = document.getElementById("code");
+    var social = document.getElementById("social");
+    var sponsors = document.getElementById("sponsors");
+    var connect = document.getElementById("connect");
+    var socialMediaHeaders = document.getElementById("social-media-headers");
+
+
+    finance.style.display = "none";
+    connect.style.display = "none";
+    code.style.display = "none";
+    social.style.display = "block";
+    sponsors.style.display = "none";
+    socialMediaHeaders.style.display = "none";
+}
