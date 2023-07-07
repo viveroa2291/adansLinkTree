dde819b95e5d2ba047a9c8e8edaf2a89b0ace915
viveroa2291 <viveroa2291@uwec.edu>
2023-07-06 21:57:16 -0500

    New Song Added
    Added a new song
    
    - Cleaner Top
    - Play and Pause Audio

---
+++ music.js
@@ -0,0 +1,13 @@
+var x = document.getElementById("music");
+var musicButton = document.getElementById("music-button");
+function pauseAudio() {
+    if(musicButton.innerText != "Play Audio") {
+        x.pause();
+        musicButton.innerText = "Play Audio";  
+    }
+    else {
+        x.play();
+        musicButton.innerText = "Pause Audio";
+    }
+    
+}
