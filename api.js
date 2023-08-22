c7033f68046664e9f5d3b1c9501fa83a3677d38a
viveroa2291 <viveroa2291@uwec.edu>
2023-08-21 22:17:32 -0500

    Testing Api's
    Nothing to see here, just testing some api's.

---
+++ api.js
@@ -0,0 +1,18 @@
+window.fbAsyncInit = function() {
+    FB.init({
+        appId      : '327019136562567',
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v17.0'
+    });
+};
+  FB.api(
+    '/me',
+    'GET',
+    {"fields":"id,name"},
+    function(response) {
+        if (response && !response.error) {
+            console.log('User ID: ' + response.id);
+            console.log('User Name: ' + response.name);
+    }
+});
