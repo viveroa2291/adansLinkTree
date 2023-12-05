cd905ff76f5314a131800f7ba23ba5324fda7512
viveroa2291 <viveroa2291@uwec.edu>
2023-12-04 22:47:55 -0600

    New CSS animation
    Added a CSS animation for fun.

--- media.js
+++ media.js
@@ -1,5 +1,24 @@
-var x = document.getElementById("music");
+let x = document.getElementById("music");
 var musicButton = document.getElementById("music-button");
+
+// Check if the play() method returns a Promise and handle it
+let playPromise = x.play();
+
+if (playPromise !== undefined) {
+  playPromise.then(_ => {
+    // The media has started playing, set isPlaying to true
+    isPlaying = true;
+  }).catch(error => {
+    // There was an error or the playback was blocked, set isPlaying to false
+    isPlaying = false;
+  });
+} else {
+  // For browsers that do not support Promise-based play() method
+  isPlaying = true; // You might assume it started playing
+}
+
+// Use the isPlaying boolean as needed in your code
+console.log(isPlaying); // Output: true or false based on the play() result
 
 if(x.play()) {
     musicButton.innerText = "Pause Audio";
